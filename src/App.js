60dc270308bb388e0e4b542adcdcaa268940a0b7
RhazzXIX <jm21tonio@gmail.com>
2023-03-24 21:51:07 +0800

    Set up App component html structure and methods

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const updateScores = (e) => {
+    setScore(score + 1);
+    if (score >= bestScore) setBestScore(score + 1);
+  };
+
+  const resetScore = (e) => {
+    setScore(0);
+  };
+
   return (
     <div className="App">
+      <header>
+        <h1>Anime Memory Game</h1>
+        <p>Score: {score}</p>
+        <p>Best Score: {bestScore}</p>
+      </header>
+      <main>
+        <h4>
+          Get points by clicking on an image. Don't click on an image twice
+        </h4>
+        <div onClick={updateScores}>Naruto</div>
+        <button onClick={resetScore}>reset</button>
+      </main>
+      <footer>
+        <a href="https://github.com/RhazzXIX">
+          <img alt="GitHub Profile"></img>
+        </a>
+      </footer>
     </div>
   );
 }
